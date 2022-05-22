0484f489436c6e248a4f653f337a065633da9953
naneps <ekaprasetya2244@gmail.com>
2022-05-22 22:09:56 +0700

    done

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Crud Fire Base',
       theme: ThemeData(primarySwatch: Colors.amber),
       home: MainPage(),
     );
